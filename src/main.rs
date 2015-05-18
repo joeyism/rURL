b45b77fc26e1b7dad2d12e8f3a51b265d006e072
joeyism <sham.joey@gmail.com>
2015-05-18 19:02:34 -0400

    Takes GET with parameters

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,56 @@
+#![feature(collections)]
+#![feature(core)]
+#![warn(non_snake_case)]
+#![warn(unused_mut)]
 extern crate hyper;
 
 use std::io::Read;
+use std::env;
+use std::convert::AsRef;
 
 use hyper::Client;
+use hyper::client::response::Response;
+use hyper::client::IntoUrl;
 use hyper::header::Connection;
 
-fn main(){
-    let mut client = Client::new();
+fn print_type_of<T>(_: &T) -> () {
+    let type_name =
+        unsafe {
+            std::intrinsics::type_name::<T>()
+        };
+    println!("{}", type_name);
+}
 
-    let mut res = client.get("http://www.joeyism.com")
-        .header(Connection::close())
-        .send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-
-
+fn responseFcn(mut res: Response){
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
-
     println!("Response: {}",body);
+}
 
+fn main(){
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 {
+        let mut client = Client::new();
+
+        match args[1].to_uppercase().as_ref(){
+            "GET" => {
+                let ref url = args[2];
+                let mut res = client.get(url)
+                    .header(Connection::close())
+                    .send().unwrap();
+                assert_eq!(res.status, hyper::Ok);
+                responseFcn(res);
+            },
+            //            "POST" =>{
+            //                let mut res = client.post(args[2])
+            //                    .body(args[3])
+            //                    .send().unwrap();
+            //                assert_eq!(res.status, hyper::Ok);
+            //                responseFcn(res);
+            //            },
+            _ => println!("I don't know what this means"),
+        }
+    } else {
+        println!("Please provide valid arguments");
+    }
 }
